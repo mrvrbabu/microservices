1fd5ba466b4d9e7ca845231aeeb11c0db856ad34
mrvrbabu <mr.vrbabu@hotmail.com>
2024-04-26 16:51:59 +0530

    Adding kubernetes master and slave installation script

---
+++ kubernetes/kubernetes-setup/k8s-slaves.sh
@@ -0,0 +1,74 @@
+#!/bin/bash 
+
+# Disable swap 
+
+echo "Disabling swap" 
+
+sudo swapon --show 
+
+sudo swapoff -a 
+
+sudo sed -i '/swap/s/^/#/' /etc/fstab
+
+#cat /etc/fstab 
+
+
+echo "Setting hostname" 
+
+#sudo hostnamectl set-hostname "k8s-master"
+
+#exec bash
+
+cat <<EOF | sudo tee -a /etc/hosts 
+192.168.56.10     k8s-master
+192.168.56.11     k8s-slave1
+192.168.56.12     k8s-slave2
+EOF
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+sudo sysctl --system
+
+sudo apt-get update
+
+sudo apt-get install -y apt-transport-https ca-certificates curl
+
+sudo mkdir -v /etc/apt/keyrings
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+
+sudo apt install -y kubeadm=1.28.1-1.1 kubelet=1.28.1-1.1 kubectl=1.28.1-1.1
+
+
+sudo apt install -y docker.io
+
+
+sudo mkdir -v /etc/containerd
+
+
+sudo sh -c "containerd config default > /etc/containerd/config.toml"
+
+sudo sed -i 's/ SystemdCgroup = false/ SystemdCgroup = true/' /etc/containerd/config.toml
+
+sudo systemctl restart containerd.service
+
+sudo systemctl restart kubelet.service
+
+sudo systemctl enable kubelet.service
